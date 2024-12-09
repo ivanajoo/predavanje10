d1bc778ab51cc05a3fbb474cdf65ca31f3d16246
ivanajoo <ivana.jonjic13@gmail.com>
2024-12-09 20:38:47 +0100

    finished main python and data.json

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#neki komentar
+
+import json
+
+with open("data.json","r") as file:
+    data = json.load(file)
+    data.append({
+                "name":"Pero Jonjic",
+                "age": 56,
+                "height": 188,
+                "gender": "male"
+    },)
+print(data)
+
+with open("data.json","w") as file:
+    json.dump(data, file, indent=4)
