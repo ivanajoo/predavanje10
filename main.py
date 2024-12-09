cec292ce36b3a4885f84946daa0de147b792388f
ivanajoo <ivana.jonjic13@gmail.com>
2024-12-09 20:43:59 +0100

    * renamed data.json to user.json * added products.json without any data * changed references in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import json
 
-with open("data.json","r") as file:
+with open("data/user.json", "r") as file:
     data = json.load(file)
     data.append({
                 "name":"Pero Jonjic",
@@ -11,5 +11,5 @@ with open("data.json","r") as file:
     },)
 print(data)
 
-with open("data.json","w") as file:
+with open("data/user.json", "w") as file:
     json.dump(data, file, indent=4)
